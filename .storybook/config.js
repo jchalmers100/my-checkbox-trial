ba3a26388ecca990205840e69bd0ae9616cb1e8d
James Chalmers <James.Chalmers@FootballPools.com>
2020-03-26 14:40:19 +0000

    files deleted

--- .storybook/config.js
+++
@@ -1,7 +0,0 @@
-// .storybook/config.js
-
-import { configure, addDecorator } from '@storybook/react';
-import ThemeDecorator from './themeDecorator';
-// automatically import all files ending in *.stories.tsx
-configure(require.context('../stories', true, /\.stories\.tsx?$/), module);
-addDecorator(ThemeDecorator);
